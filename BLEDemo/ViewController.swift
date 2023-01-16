de035d58b94d30cfaba53e28931e61da17938055
kenneth.wu <kenneth.wu@ogglobal.net>
2023-01-16 18:56:18 +0800

    First push. Create init. project.

---
+++ BLEDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLEDemo
+//
+//  Created by KennethWu on 2023/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
